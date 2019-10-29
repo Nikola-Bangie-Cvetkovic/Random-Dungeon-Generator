4c69b0b34e5cb9f4b0e543757371de2ee2975f51
Nikola Z Cvetkovic <nikolacvetkovic990@gmail.com>
2019-10-29 13:25:39 +0100

    init

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class LevelGenerator : MonoBehaviour
+{
+    //0 UP
+    //1 RIGHT
+    //2 DOWN
+    //3 RIGHT 
+    public int doorDirection;
+    private bool spawned;
+    private LevelTemplate templates;
+    private Transform level;
+
+    void Start()
+    {
+        spawned = false;
+        level = GameObject.FindWithTag("Level").transform;
+        templates = GameObject.FindWithTag("LvlTemp").GetComponent<LevelTemplate>();
+        Invoke("Spawn", 1f);
+    }
+
+    void Spawn()
+    {
+        if (!spawned)
+        {
+            if (doorDirection == 0)
+            {
+                int r = Random.Range(0, templates.roomsWithDownDoors.Length);
+                Transform room = Instantiate(templates.roomsWithDownDoors[r],
+                    transform.position, templates.roomsWithDownDoors[r].rotation);
+                room.parent = level;
+            }
+            else if (doorDirection == 1)
+            {
+                int r = Random.Range(0, templates.roomsWithLeftDoors.Length);
+                Transform room = Instantiate(templates.roomsWithLeftDoors[r],
+                    transform.position, templates.roomsWithLeftDoors[r].rotation);
+                room.parent = level;
+            }
+            else if (doorDirection == 2)
+            {
+                int r = Random.Range(0, templates.roomsWithUpDoors.Length);
+                Transform room = Instantiate(
+                    templates.roomsWithUpDoors[r],
+                    transform.position, templates.roomsWithUpDoors[r].rotation);
+                room.parent = level;
+            }
+            else if (doorDirection == 3)
+            {
+                int r = Random.Range(0, templates.roomsWithRightDoors.Length);
+                Transform room = Instantiate(
+                    templates.roomsWithRightDoors[r],
+                    transform.position, templates.roomsWithRightDoors[r].rotation);
+                room.parent = level;
+            }
+
+            spawned = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "SpawnPoint")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/LevelTemplate.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+public class LevelTemplate : MonoBehaviour
+{
+    public Transform[] roomsWithUpDoors;
+    public Transform[] roomsWithRightDoors;
+    public Transform[] roomsWithDownDoors;
+    public Transform[] roomsWithLeftDoors;
+}
